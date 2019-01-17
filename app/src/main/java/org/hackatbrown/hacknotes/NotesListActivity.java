7ceeec527ddad1d9a6b793d93b4f35eaecfeeaf6
Ben Navetta <ben.navetta@gmail.com>
2019-01-16 23:56:29 -0500

    Checkpoint 2: Handlers

--- app/src/main/java/org/hackatbrown/hacknotes/NotesListActivity.java
+++ app/src/main/java/org/hackatbrown/hacknotes/NotesListActivity.java
@@ -2,6 +2,8 @@ package org.hackatbrown.hacknotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class NotesListActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class NotesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+    }
+
+    public void onNewNote(View button) {
+        Toast.makeText(getApplicationContext(), "Creating note!", Toast.LENGTH_SHORT).show();
     }
 }
